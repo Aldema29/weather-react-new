6dad90cfd44b52690243ef1fe82622de27ce8273
Aldema29 <aldema29.am@gmail.com>
2024-09-01 11:30:08 -0400

    updates

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,14 @@
 import React from "react";
+import { Audio } from 'react-loader-spinner'
+;<Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="three-dots-loading"
+  wrapperStyle
+  wrapperClass
+/>
 
 export default function Weather() {
     return <h2> Hello from my Weather App </h2>
