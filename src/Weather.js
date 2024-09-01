e9b6c3aec163af5391bd5d80cec4a6abc3723f9b
Aldema29 <aldema29.am@gmail.com>
2024-09-01 11:44:14 -0400

    CodeSandboxReact project to VS Code

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,55 @@
 import React from "react";
 
 export default function Weather() {
-    return <h2> Hello from my Weather App </h2>
+  return (
+    <div>
+      <div class="weather-app">
+        <header>
+          <form class="search-form" id="search-form">
+            <input
+              class="search-form-input"
+              id="search-form-input"
+              type="search"
+              placeholder="Enter a city..."
+              required
+            />
+            <input class="search-form-button" type="submit" value="Search" />
+          </form>
+        </header>
+
+        <main>
+          <div class="weather-app-data">
+            <div>
+              <h1 class="weather-app-city" id="city">
+                New York, NY
+              </h1>
+              <p class="weather-app-details">
+                <span id="time">Sunday 12:00</span>,{" "}
+                <span id="description">sunny</span>
+                <br />
+                Humidity:{" "}
+                <strong>
+                  <span id="humidity">35</span>%
+                </strong>
+                , Wind:{" "}
+                <strong>
+                  <span id="wind-speed">5</span>mph
+                </strong>
+              </p>
+            </div>
+            <div class="weather-app-temperature-container">
+              <div class="weather-app-icon" id="icon">
+                ☀️
+              </div>
+              <div class="weather-app-temperature-value" id="temperature">
+                85
+              </div>
+              <div class="weather-app-unit">&deg;F</div>
+            </div>
+          </div>
+        </main>
+        <footer></footer>
+      </div>
+    </div>
+  );
 }
